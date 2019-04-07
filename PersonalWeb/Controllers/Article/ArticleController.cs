6c5333fec532eb3cb00281239af1162b6f3223ab
LYF <LYF@DESKTOP-AI4R017>
2019-04-07 21:31:50 +0800

    1

---
+++ PersonalWeb/Controllers/Article/ArticleController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWeb.Controllers.Article
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+
+        /// <summary>
+        /// 文章列表页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 文章详情页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersonalWeb/Controllers/Home/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 主框架
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 右侧容器
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult RightContainer()
+        {
+            return View();
+        }
+    }
+}
